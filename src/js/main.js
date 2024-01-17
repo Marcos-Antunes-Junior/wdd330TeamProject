e2fa4b8328bca31d6a60b7d8d86915dc452563ea
Aaron Webster <aaron.thomas.webster@gmail.com>
2024-01-16 22:02:05 -0700

    updated main.js to remove top level await.

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,25 @@
 import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData('tents');
-const tents = await dataSource.getData();
 
-console.log({tents});
-const topTentIds = ['880RR','985RF','985PR','344YJ']
-const productList =  document.querySelector('.product-list')
-const productCardTemplate = document.querySelector('#product-list__card-template');
-tents.filter(tent => topTentIds.includes(tent.Id)).forEach(product => {
-  const clone = productCardTemplate.content.cloneNode(true);
-  clone.querySelector('.card_product_page').href = `product_pages/index.html?product=${product.Id}`;
-  clone.querySelector('.card__image').src = product.Image.substring(1);
-  clone.querySelector('.card__image').alt = product.NameWithoutBrand;
-  clone.querySelector('.card__name').textContent = product.NameWithoutBrand;
-  clone.querySelector('.card__brand').textContent = product.Brand.Name;
-  clone.querySelector('.product-card__price').textContent = product.FinalPrice;
-  productList.appendChild(clone);
-});
+
+const dataSource = new ProductData('tents');
+
+const init = async () => {
+  const tents = await dataSource.getData();
+
+  console.log({ tents });
+  const topTentIds = ['880RR', '985RF', '985PR', '344YJ']
+  const productList = document.querySelector('.product-list')
+  const productCardTemplate = document.querySelector('#product-list__card-template');
+  tents.filter(tent => topTentIds.includes(tent.Id)).forEach(product => {
+    const clone = productCardTemplate.content.cloneNode(true);
+    clone.querySelector('.card_product_page').href = `product_pages/index.html?product=${product.Id}`;
+    clone.querySelector('.card__image').src = product.Image.substring(1);
+    clone.querySelector('.card__image').alt = product.NameWithoutBrand;
+    clone.querySelector('.card__name').textContent = product.NameWithoutBrand;
+    clone.querySelector('.card__brand').textContent = product.Brand.Name;
+    clone.querySelector('.product-card__price').textContent = product.FinalPrice;
+    productList.appendChild(clone);
+  });
+}
+init();
