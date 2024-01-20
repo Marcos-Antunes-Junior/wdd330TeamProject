6ac8c497b7606fd71fcccf4d0e3b3bd654cad873
Aaron Webster <aaron.thomas.webster@gmail.com>
2024-01-20 10:19:00 -0700

    updated code to match the assignment instructions

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,9 @@
 import ProductData from './ProductData.mjs';
-
-
+import ProductListing from './ProductList.mjs';
 
 const dataSource = new ProductData('tents');
+const element = document.querySelector('.product-list');
+const topTentIds = ['880RR', '985RF', '985PR', '344YJ'];
+const listing = new ProductListing('Tents', dataSource , element, topTentIds);
 
-const init = async () => {
-  const tents = await dataSource.getData();
-
-  console.log({ tents });
-  const topTentIds = ['880RR', '985RF', '985PR', '344YJ']
-  const productList = document.querySelector('.product-list')
-  const productCardTemplate = document.querySelector('#product-list__card-template');
-  tents.filter(tent => topTentIds.includes(tent.Id)).forEach(product => {
-    const clone = productCardTemplate.content.cloneNode(true);
-    clone.querySelector('.card_product_page').href = `product_pages/index.html?product=${product.Id}`;
-    clone.querySelector('.card__image').src = product.Image.substring(1);
-    clone.querySelector('.card__image').alt = product.NameWithoutBrand;
-    clone.querySelector('.card__name').textContent = product.NameWithoutBrand;
-    clone.querySelector('.card__brand').textContent = product.Brand.Name;
-    clone.querySelector('.product-card__price').textContent = product.FinalPrice;
-    productList.appendChild(clone);
-  });
-}
-init();
+listing.init();
