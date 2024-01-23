27e95ab4a230ff43464e0fcffb00cb18ed66d39f
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2024-01-22 22:59:50 -0300

    "added cart quantities feature"

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-import ProductData from './ProductData.mjs';
-import ProductListing from './ProductList.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
 
-const dataSource = new ProductData('tents');
-const element = document.querySelector('.product-list');
-const topTentIds = ['880RR', '985RF', '985PR', '344YJ'];
-const listing = new ProductListing('Tents', dataSource , element, topTentIds);
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const topTentIds = ["880RR", "985RF", "985PR", "344YJ"];
+const listing = new ProductListing("Tents", dataSource, element, topTentIds);
 
 listing.init();
