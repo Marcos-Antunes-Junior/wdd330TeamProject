c205296b0f5ef822248a739d579811572c9304d2
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2024-01-29 17:59:54 -0300

    "fixed tent images"

---
+++ src/js/product-listing.js
@@ -0,0 +1,14 @@
+/*import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";*/
+import {loadHeaderFooter } from './utils.mjs';
+
+/*
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const topTentIds = ["880RR", "985RF", "985PR", "344YJ"];
+const listing = new ProductListing("Tents", dataSource, element, topTentIds);
+
+listing.init();
+*/
+
+loadHeaderFooter();
